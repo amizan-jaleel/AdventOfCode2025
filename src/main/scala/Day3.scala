d2b7a45f78f0c6d4384c18152a61566d026ace3f
Amizan Jaleel <najm555@gmail.com>
2025-12-27 19:35:51 -0500

    Day 3, functional programming

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -12,6 +12,24 @@ object Day3 {
     curMax
   }
 
+  def maxJoltsPerBankFunctional(input: String): Long = input
+    .split("\\s*\\n\\s*")
+    .view
+    .map(_.trim)
+    .filter(_.nonEmpty)
+    .map { line =>
+      (0 until line.length -1)
+        .view
+        .map { i =>
+          (i + 1 until line.length)
+            .map(j => s"${line.charAt(i)}${line.charAt(j)}".toLong)
+            .max
+        }
+        .max
+    }
+    .sum
+
+
   def day3(inputs: List[String]): Long =
-    inputs.map(maxJoltsPerBank).sum
+    inputs.map(maxJoltsPerBankFunctional).sum
 }
